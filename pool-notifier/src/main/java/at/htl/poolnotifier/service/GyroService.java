d1dbc38028984f7524ae78e9039284119859f273
ninaholzinger <n.holzinger@students.htl-leonding.ac.at>
2020-02-02 13:44:59 +0100

    Parse consumed data to Object with JsonB and send it as stream instead of a normal object

--- pool-notifier/src/main/java/at/htl/poolnotifier/service/GyroService.java
+++ pool-notifier/src/main/java/at/htl/poolnotifier/service/GyroService.java
@@ -1,19 +1,40 @@
 package at.htl.poolnotifier.service;
 
 import at.htl.poolnotifier.entity.GyroData;
+import at.htl.poolnotifier.repository.GyroDataRepository;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.eclipse.microprofile.reactive.streams.operators.ReactiveStreams;
+import org.reactivestreams.Publisher;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+import javax.json.bind.JsonbException;
 
 @ApplicationScoped
 public class GyroService {
 
+    @Inject
+    GyroDataRepository gyroDataRepository;
+
+    Jsonb jsonb;
+
+    public GyroService() {
+        jsonb = JsonbBuilder.create();
+    }
+
     @Incoming("pool")
     @Outgoing("pool-alarm")
-    public GyroData validateData(byte[] rawData) {
+    public Publisher<GyroData> consumeData(byte[] rawData) {
         String data = new String(rawData);
-        System.out.println(data);
-        return new GyroData(0, 0, 0, "pool1");
+        GyroData gyroData = null;
+        try {
+            gyroData = jsonb.fromJson(data, GyroData.class);
+        } catch (JsonbException ignored) {
+        }
+
+        return ReactiveStreams.ofNullable(gyroData).buildRs();
     }
 }
