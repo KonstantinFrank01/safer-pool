babec9a8723f319fbbed039c9806ee0f79fef50b
ninaholzinger <n.holzinger@students.htl-leonding.ac.at>
2020-02-02 14:46:36 +0100

    Add method to show all data

--- pool-notifier/src/main/java/at/htl/poolnotifier/boundary/GyroDataEndpoint.java
+++ pool-notifier/src/main/java/at/htl/poolnotifier/boundary/GyroDataEndpoint.java
@@ -1,26 +1,38 @@
 package at.htl.poolnotifier.boundary;
 
 import at.htl.poolnotifier.entity.GyroData;
+import at.htl.poolnotifier.repository.GyroDataRepository;
 import io.smallrye.reactive.messaging.annotations.Channel;
 import org.reactivestreams.Publisher;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("pool")
 public class GyroDataEndpoint {
+
+    @Inject
+    GyroDataRepository gyroDataRepository;
 
     @Inject
     @Channel("pool-alarm")
     Publisher<GyroData> alarmData;
 
     @GET
-    public String hello() {
-        return "Hello World";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllData(@QueryParam("limit") Integer limit) {
+        if (limit == null || limit < 0) {
+            return Response.ok(gyroDataRepository.getAllData()).build();
+        }
+        return Response.ok(gyroDataRepository.getAllData(limit)).build();
     }
+
 
     @GET
     @Path("/stream")
