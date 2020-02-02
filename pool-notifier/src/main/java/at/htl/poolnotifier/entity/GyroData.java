12a1b76ea363420d035879891560ec5333820210
ninaholzinger <n.holzinger@students.htl-leonding.ac.at>
2020-02-02 01:39:54 +0100

    Add getter and setter

--- pool-notifier/src/main/java/at/htl/poolnotifier/entity/GyroData.java
+++ pool-notifier/src/main/java/at/htl/poolnotifier/entity/GyroData.java
@@ -18,4 +18,44 @@ public class GyroData extends PanacheEntity {
         this.location = location;
         this.notificationTime = LocalDateTime.now();
     }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public LocalDateTime getNotificationTime() {
+        return notificationTime;
+    }
+
+    public void setNotificationTime(LocalDateTime notificationTime) {
+        this.notificationTime = notificationTime;
+    }
 }
