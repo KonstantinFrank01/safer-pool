ee7369d81e95cb9df50a83ca964199e59e3414b4
ninaholzinger <n.holzinger@students.htl-leonding.ac.at>
2020-03-21 16:03:47 +0100

    Implement deactivation

--- pool-notifier/src/main/java/at/htl/poolnotifier/service/DeactivationService.java
+++ pool-notifier/src/main/java/at/htl/poolnotifier/service/DeactivationService.java
@@ -1,4 +1,31 @@
 package at.htl.poolnotifier.service;
 
+import javax.enterprise.context.ApplicationScoped;
+import java.util.Timer;
+import java.util.TimerTask;
+
+@ApplicationScoped
 public class DeactivationService {
+
+    private boolean isDeactivated;
+
+    public boolean isDeactivated() {
+        return isDeactivated;
+    }
+
+    public void setDeactivated(boolean deactivated) {
+        isDeactivated = deactivated;
+    }
+
+    public void deactivate(int seconds){
+        Timer timer = new Timer();
+        isDeactivated = true;
+
+        timer.schedule(new TimerTask(){
+            @Override
+            public void run() {
+                isDeactivated = false;
+            }
+        }, seconds * 1000);
+    }
 }
